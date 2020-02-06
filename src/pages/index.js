cd0902a5a7d8d211a0a0b449473b1304e8fd9cad
darrinim <darrinj.im@gmail.com>
2020-02-06 15:41:46 -0500

    took out contact link on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Darrin, I am trying out Gatsby. Wish me luck!</h2>
-      <p>Need a developer? 
-        <Link to="/contact">Contact</Link>
-      </p>
     </Layout>
   )
 };
